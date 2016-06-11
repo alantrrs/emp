c4a1be827a868a3a449f5d10429998a10cfcbad8
alantrrs <alan.trrs.ngls@gmail.com>
2016-06-10 19:38:23 -0700

    Getting started

---
+++ listen.js
@@ -0,0 +1,47 @@
+
+var Pusher = require('pusher-js/node')
+var config = require('./config')
+var emp = require('./lib')
+var debug = require('debug')('emp')
+
+var auth = new Buffer(`${config.client.key}:${config.client.secret}`).toString('base64')
+var headers = {
+  'Authorization': 'Basic ' + auth
+}
+
+var pusher = new Pusher(config.pusher.key, {
+  authEndpoint: config.pusher.auth_endpoint,
+  auth: {
+    headers: headers
+  }
+})
+
+const channel = `private-${config.client.key}-tasks`
+var tasks_channel = pusher.subscribe(channel)
+
+// Queue
+var queue = []
+console.log('EMP is listening for tasks on channel:', channel)
+tasks_channel.bind('new-task', function (task) {
+  debug('Received task: %o', task)
+  queue.push(task)
+})
+
+// Consume
+function consume() {
+  var task = queue.shift()
+  if (task) {
+    debug('Running task: %o', task)
+    return emp.runTask(task).then(function () {
+      console.log('SUCCESS')
+      consume()
+    }).catch(function (err) {
+      console.log('ERROR: ', err)
+      consume()
+    })
+  } else {
+    setTimeout(consume, 1000)
+  }
+}
+
+consume()
